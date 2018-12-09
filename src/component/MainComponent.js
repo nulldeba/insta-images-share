e360eeb4abbce65b9128a47322236a3b0f091020
nulldeba <nulldebashis@gmail.com>
2018-12-09 18:22:38 +0530

    Add image feature added

--- src/component/MainComponent.js
+++ src/component/MainComponent.js
@@ -1,31 +1,86 @@
-import React,{Component} from 'react';
-class Main extends Component{
+import React, { Component } from 'react';
+import { UserPosts } from './UserPostsComponent';
+class Main extends Component {
 
-    constructor(props)
-    {
+    constructor(props) {
         super(props);
-        this.state={
-            id:1,
-            name:'Debashis Singh',
-            phone:'7978104553',
-            posts:[]
+        this.state = {
+            id: 1,
+            name: 'Debashis Singh',
+            phone: '7978104553',
+            posts: []
         }
     }
-    saveToLocalStorage()
-    {
-        localStorage.setItem("user",this.state);
+    saveToLocalStorage() {
+        localStorage.setItem("user", JSON.stringify(this.state));
+
     }
-    componentDidMount()
-    {
-        fetch('http://starlord.hackerearth.com/insta')
-      .then(result => result.json())
-      .then((result) => {
-        this.setState({posts:result});
-      });
+    onChange(e) {
+        let files = e.target.files;
+        let reader = new FileReader();
+        reader.readAsDataURL(files[0]);
+        let date = new Date();
+        if (files[0].size < 5000000)
+            reader.onload = (e) => {
+                let posts = this.state.posts;
+                let postItem = {
+                    image: e.target.result,
+                    likes: 0,
+                    comments: [],
+                    timestamp: date,
+                    id: posts.length
+                }
+                posts.push(postItem);
+                this.setState({ posts: posts });
+                this.saveToLocalStorage();
+            }
+
     }
-    render(){
-        return(<div>
-            </div>);
+    componentDidMount() {
+        let userData = JSON.parse(localStorage.getItem('user'));
+        if (userData.posts.length == 0) {
+            fetch('http://starlord.hackerearth.com/insta')
+                .then(result => result.json())
+                .then((results) => {
+                    let posts = [];
+                    results.map((post, index) => {
+                        posts.push({
+                            id: index,
+                            image: post.Image,
+                            likes: post.likes,
+                            timestamp: post.timestamp,
+                            comments: []
+                        })
+                        return;
+                    });
+
+                    this.setState({ posts: posts });
+                    this.saveToLocalStorage();
+                });
+        }
+        else {
+            this.setState({ posts: userData.posts });
+        }
+
+
+
+    }
+    userPosts() {
+
+    }
+    render() {
+        let userPosts;
+        if (this.state.posts)
+            userPosts = <UserPosts posts={this.state.posts} />
+        else
+            userPosts = <div></div>;
+        return (<div>
+            Main Component
+            <input type="file" name="file" onChange={(e) => { this.onChange(e) }} />
+            <div>
+                {userPosts}
+            </div>
+        </div>);
     }
 }
 export default Main;
--- src/component/UserPostsComponent.js
+++ src/component/UserPostsComponent.js
@@ -4,8 +4,27 @@ import {
     CardTitle, Breadcrumb, BreadcrumbItem
 } from 'reactstrap';
 
-const UserPost=(props)=>{
-    const post=props.post.map((post)=>{
-        
+function RenderPostItem({ post, onClick }) {
+    return (
+        <Card>
+            {/* <Link to={`/menu/${dish.id}`} > */}
+                <CardImg width="100%" height="100%" src={post.image} alt="instagram" />
+                <CardImgOverlay>
+                </CardImgOverlay>
+            {/* </Link> */}
+        </Card>
+    );
+}
+export const UserPosts=(props)=>{
+    const posts=props.posts.map((post)=>{
+        return (
+           
+            <div className="col-4 col-sm-4" key={post.id}>
+                <RenderPostItem post={post} />
+           
+            </div>
+            
+        );
     })
+    return ( <div className="container"><div className="row">{posts}</div></div>)
 }
